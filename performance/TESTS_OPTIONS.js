c8d70de0d6ee81ad96a320eda78beed75cd070b7
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-23 19:27:20 -0700

    Update test options

--- performance/TESTS_OPTIONS.js
+++ performance/TESTS_OPTIONS.js
@@ -1,20 +1,21 @@
-'use strict';
+"use strict";
 
 // const CONCURRENCIES = [10000];
 // const CONCURRENCIES = [100];
 // const CONCURRENCIES = [1];
 const CONCURRENCIES = [1, 100, 1000, 10000];
-const ALWAYS_STATS = [false, true];
+// const ALWAYS_STATS = [false, true];
+const ALWAYS_STATS = [false];
 const TESTS_OPTIONS = [];
 
 for (const alwaysStat of ALWAYS_STATS) {
   for (const concurrency of CONCURRENCIES) {
     TESTS_OPTIONS.push({
-      name: `${concurrency}-${alwaysStat ? 't' : 'f'}`,
+      name: `${concurrency}-${alwaysStat ? "t" : "f"}`,
       options: { highWaterMark: concurrency, alwaysStat },
     });
   }
 }
-TESTS_OPTIONS.push({ name: 'default' });
+TESTS_OPTIONS.push({ name: "default" });
 
 module.exports = TESTS_OPTIONS;
