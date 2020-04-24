217412e6a4812f04a1b37b356c13d61c93a5a623
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-23 22:02:26 -0700

    Moved eslint to external file for pickup by vscode

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: 'script',
+  },
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'array-callback-return': 'error',
+    'no-empty': [
+      'error',
+      {
+        allowEmptyCatch: true,
+      },
+    ],
+    'no-else-return': [
+      'error',
+      {
+        allowElseIf: false,
+      },
+    ],
+    'no-lonely-if': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-arrow-callback': [
+      'error',
+      {
+        allowNamedFunctions: true,
+      },
+    ],
+    'prefer-const': [
+      'error',
+      {
+        ignoreReadBeforeAssign: true,
+      },
+    ],
+    'prefer-destructuring': [
+      'error',
+      {
+        object: true,
+        array: false,
+      },
+    ],
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    radix: 'error',
+    semi: 'error',
+    strict: 'error',
+    quotes: ['error', 'single'],
+  },
+};
